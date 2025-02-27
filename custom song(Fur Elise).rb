9b3297e410070edf3889028e957101577c16b9a6
sebastian12783 <sebastian_camacho@student.davincischools.org>
2025-02-27 12:13:03 -0800

    Update custom song(Fur Elise).rb

--- custom song(Fur Elise).rb
+++ custom song(Fur Elise).rb
@@ -6,44 +6,87 @@ live_loop :beat do
   # First part of the melody
   #total sleep= 2
   play :e3
-  sleep 0.7
+  sleep 0.5
   play :d3
-  sleep 0.7
+  sleep 0.5
   play :e3
-  sleep 0.7
+  sleep 0.5
   play :d3
-  sleep 0.7
+  sleep 0.5
   play :e3
-  sleep 0.7
+  sleep 0.5
   play :b3
-  sleep 0.7
+  sleep 0.5
+  
+  play :d3
+  sleep 0.5
+  play :c3
+  sleep 0.5
+  play :a2
+  sleep 1.5
+  play :c4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :b4
+  sleep 1.5
+  play :e4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :c5
+  sleep 2
+  #repeat
+  play :e3
+  sleep 0.5
+  play :d3
+  sleep 0.5
+  play :e3
+  sleep 0.5
+  play :d3
+  sleep 0.5
+  play :e3
+  sleep 0.5
+  play :b3
+  sleep 0.5
   
   # Second part of the melody
   #total sleep= 2
   play :d3
-  sleep 0.7
+  sleep 0.5
   play :c3
-  sleep 0.7
+  sleep 0.5
   play :a2
-  sleep 2
+  sleep 1.5
   play :c4
-  sleep 0.7
+  sleep 0.5
   play :e4
-  sleep 0.7
+  sleep 0.5
   play :a4
-  sleep 2
+  sleep 0.5
   play :b4
-  sleep 0.7
+  sleep 1.5
   play :e4
-  sleep 0.7
+  sleep 0.5
   play :g4
-  sleep 0.7
-  play :c4
-  sleep 0.7
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :c5
+  sleep 0.5
 end
 #total sleep=8.75
-sleep 8
-live_loop :drm do
-  sample :drum_bass_hard
-  sleep 0.75
+sleep 6
+play :a2
+sleep 1.1
+play :e3
+sleep 1.1
+play :a3
+sleep 1.1
+play :a2
 end
+sample :ambi_choir
