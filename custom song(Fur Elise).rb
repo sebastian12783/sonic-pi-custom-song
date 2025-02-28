a601d08682c3c831feaa45313c45c5d4b3b6a2da
sebastian12783 <sebastian_camacho@student.davincischools.org>
2025-02-28 12:11:57 -0800

    Update custom song(Fur Elise).rb

--- custom song(Fur Elise).rb
+++ custom song(Fur Elise).rb
@@ -1,10 +1,20 @@
 # Simple version of the Fur Elise
+use_bpm 160
+use_synth :square
+
+live_loop :choir do
+  32.times do
+    sample :ambi_choir,beat_stretch: 7
+    sleep 1
+  end
+  stop
+end
+sleep 8
 live_loop :beat do
   
-  use_bpm 160
-  use_synth :square
   # First part of the melody
   #total sleep= 2
+  sleep 8
   play :e3
   sleep 0.5
   play :d3
@@ -17,6 +27,7 @@ live_loop :beat do
   sleep 0.5
   play :b3
   sleep 0.5
+  print("one")
   
   play :d3
   sleep 0.5
@@ -40,6 +51,7 @@ live_loop :beat do
   sleep 0.5
   play :c5
   sleep 2
+  print("two")
   #repeat
   play :e3
   sleep 0.5
@@ -53,7 +65,7 @@ live_loop :beat do
   sleep 0.5
   play :b3
   sleep 0.5
-  
+  print("three")
   # Second part of the melody
   #total sleep= 2
   play :d3
@@ -78,15 +90,16 @@ live_loop :beat do
   sleep 0.5
   play :c5
   sleep 0.5
+  stop
 end
-#total sleep=8.75
-sleep 6
-play :a2
-sleep 1.1
-play :e3
-sleep 1.1
-play :a3
-sleep 1.1
-play :a2
+sleep 18
+live_loop :choir2 do
+  8.times do
+    sample :ambi_choir,beat_stretch: 4, amp: 2
+    sleep 1
+  end
+  stop
 end
-sample :ambi_choir
+
+sleep 12
+sample "C:/Users/sebastian_camacho/Documents/Audacity/_newMain Theme - Super Smash Bros Brawl.wav"
